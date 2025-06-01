6c536b5527599e6dd41bf26e8b5a6d83b151be04
RohanPengonda <rpengonda1@gmail.com>
2025-06-01 22:29:05 +0530

    initial

--- server/server.js
+++ server/server.js
@@ -13,6 +13,9 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 
+app.get('/', (req, res) => {
+  res.json("Hey!! Welcome to Server")
+})
 try {
   mongoose.connect(process.env.MONGO_URI)
   console.log("Database Connected")
